0188757a171e830340859b0683e47d94504f8209
Jiub255 <113727475+Jiub255@users.noreply.github.com>
2022-12-24 00:26:58 -0800

    Transparentizer working well
    Still might tweak the lerp in the fade coroutine but it looks good as is

--- Assets/Scripts/Game Stuff/Transparentizer.cs
+++ Assets/Scripts/Game Stuff/Transparentizer.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 // Put this on camera
 public class Transparentizer : MonoBehaviour
@@ -14,23 +13,21 @@ public class Transparentizer : MonoBehaviour
     // Currently selected PC
     private Transform currentPlayerCharacterTransform;
 
+    // A material should be on at most one of these next three lists/dicts at once
     // List of all materials that are currently fully faded out
     private List<Material> fadedOut = new List<Material>();
 
     // Dictionary of all materials/coroutines that are currently fading
-    private Dictionary<Material, Coroutine> currentlyFadingDict = new Dictionary<Material, Coroutine>();
+    private Dictionary<Material, Coroutine> fadingOutDict = new Dictionary<Material, Coroutine>();
+
+    // Dictionary of all materials/coroutines that are currently fading
+    private Dictionary<Material, Coroutine> fadingInDict = new Dictionary<Material, Coroutine>();
 
     [Tooltip("Length of the fade")]
     [SerializeField, Range(0, 2f)] private float fadeDuration = 1f;
 
     [Tooltip("Opacity of the object when fully faded")]
     [SerializeField, Range(0, 1f)] private float minimalOpacity = 0.1f;
-
-    private void Awake()
-    {
-/*        playerInput = GetComponent<PlayerInput>();
-        mousePositionAction = playerInput.actions["MousePosition"];*/
-    }
 
     private void OnEnable()
     {
@@ -72,7 +69,7 @@ public class Transparentizer : MonoBehaviour
             hits = selectedPlayerHits.Concat(hits).ToArray();
         }
 
-        // List gets remade each fixed update
+        // List of all in the way materials, gets remade each fixed update
         List<Material> inTheWayMaterials = new List<Material>();
 
         foreach (RaycastHit hit in hits)
@@ -81,29 +78,39 @@ public class Transparentizer : MonoBehaviour
 
             foreach (Material material in hitMaterials)
             {
-                // Adds all in the way materials to list, and fades them if they're not already fading/faded
-                inTheWayMaterials.Add(AndFadeIfNecessary(material));
+                // Adds all in the way materials to list, and starts fading them if they're not already fading/faded
+                // Also stops fading-in coroutines and starts fading those materials out
+                inTheWayMaterials.Add(AndFadeOutIfNecessary(material));
             }
         }
 
-        // If a material is faded out but no longer in the way, unfade it
+        // If a material is faded out or fading out but no longer in the way, unfade it
         List<Material> materialsToUnfade = new List<Material>();
 
-        foreach (Material material in fadedOut)
+        for (int i = fadedOut.Count - 1; i >= 0; i--)
         {
-            if (!inTheWayMaterials.Contains(material))
+            if (!inTheWayMaterials.Contains(fadedOut[i]))
             {
-                // TODO: check for materials which are currently still fading, then stop those coroutines
-                    // Keep a dictionary of <Material, Coroutine>? Dictionary<Material, Coroutine> materialCoroutineKVPs = new Dictionary<Material, Coroutine>();
-                    // then stop the ones associated with the still fading materials that aren't in the way anymore?
-                // If a material is faded out but no longer in the way, unfade it
-                materialsToUnfade.Add(AndUnfade(material));
+                // If a material is FADED out but no longer in the way, unfade it
+                materialsToUnfade.Add(AndUnfade(fadedOut[i]));
+                fadedOut.Remove(fadedOut[i]);
             }
         }
 
-        foreach (Material material in materialsToUnfade)
+        List<KeyValuePair<Material, Coroutine>> itemsToRemove = new List<KeyValuePair<Material, Coroutine>>();
+        foreach (KeyValuePair<Material, Coroutine> kvp in fadingOutDict)
         {
-            fadedOut.Remove(material);
+            if (!inTheWayMaterials.Contains(kvp.Key))
+            {
+                // If a material is FADING out but no longer in the way, unfade it
+                itemsToRemove.Add(kvp);
+            }
+        }
+        foreach (KeyValuePair<Material, Coroutine> kvp in itemsToRemove)
+        {
+            StopCoroutine(kvp.Value);
+            materialsToUnfade.Add(AndUnfade(kvp.Key));
+            fadingOutDict.Remove(kvp.Key);
         }
     }
 
@@ -113,6 +120,7 @@ public class Transparentizer : MonoBehaviour
         List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
 
         meshRenderers.AddRange(hit.collider.GetComponents<MeshRenderer>());
+        // Uncomment below line if using objects with colliders on grandchildren
         //meshRenderers.AddRange(hit.collider.GetComponentsInChildren<MeshRenderer>());
 
         if (meshRenderers.Count > 0)
@@ -126,15 +134,26 @@ public class Transparentizer : MonoBehaviour
         return materials;
     }
 
-    private Material AndFadeIfNecessary(Material material)
+    private Material AndFadeOutIfNecessary(Material material)
     {
-        if (!fadedOut.Contains(material))
+        // If material isn't faded out or fading out, ...
+        if (!fadedOut.Contains(material) && !fadingOutDict.ContainsKey(material))
         {
-            fadedOut.Add(material);
+            // If material is fading in, stop that coroutine and remove it from dictionary
+            if (fadingInDict.ContainsKey(material))
+            {
+                StopCoroutine(fadingInDict[material]);
+                fadingInDict.Remove(material);
+            }
 
+            // Start fade out coroutine
             Color fadeColor = new Color(
                 material.color.r, material.color.g, material.color.b, minimalOpacity);
-            StartCoroutine(Fade(material, fadeColor, fadeDuration));
+
+            Coroutine coroutine = StartCoroutine(Fade(material, fadeColor, fadeDuration));
+
+            // Add to fadingOutDict, then add to faded list when coroutine done
+            fadingOutDict.Add(material, coroutine);
 
             return material;
         }
@@ -148,24 +167,43 @@ public class Transparentizer : MonoBehaviour
     {
         Color originalColor = new Color(
             material.color.r, material.color.g, material.color.b, 1f);
-        StartCoroutine(Fade(material, originalColor, fadeDuration));
+
+        Coroutine coroutine = StartCoroutine(Fade(material, originalColor, fadeDuration));
+
+        // Add to fadingInDict. Remove from fadingInDict when coroutine done
+        fadingInDict.Add(material, coroutine);
+
         return material;
     }
 
     private IEnumerator Fade(Material material, Color fadedColor, float duration)
     {
-       // Debug.Log("Fade coroutine started");
-
         float time = 0f;
         Color regularColor = material.color;
 
         while (time < duration)
         {
+            // TODO: Fix this lerp. Use idea from CameraControllerFollower comments
             material.color = Color.Lerp(material.color, fadedColor, time);
             time += Time.deltaTime;
             yield return null;
         }
 
         material.color = fadedColor;
+
+        // If material is fading out (not in), ...
+        if (fadingOutDict.ContainsKey(material))
+        {
+            // Remove from fadingOutDict
+            fadingOutDict.Remove(material);
+
+            // Add to faded list
+            fadedOut.Add(material);
+        }
+        else if (fadingInDict.ContainsKey(material))
+        {
+            // Remove from fadingInDict
+            fadingInDict.Remove(material);
+        }
     }
 }
